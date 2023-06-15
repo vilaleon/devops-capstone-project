96f6a203597e5919b4df2d1d920af05299806d9f
vilaleon <>
2023-06-15 07:14:35 -0400

    Complete CI build

--- service/routes.py
+++ service/routes.py
@@ -55,6 +55,7 @@ def create_accounts():
         jsonify(message), status.HTTP_201_CREATED, {"Location": location_url}
     )
 
+
 ######################################################################
 # LIST ALL ACCOUNTS
 ######################################################################
